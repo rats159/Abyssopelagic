574caf97c883e1a5c5e9ce87a8cfc1ab0fe2694e
rats159 <113955400+rats159@users.noreply.github.com>
2024-09-13 03:41:14 -0500

    Initial commit

---
+++ src/main/java/dev/rats159/abyssopelagic/AbyssopelagicClient.java
@@ -0,0 +1,11 @@
+package dev.rats159.abyssopelagic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AbyssopelagicClient implements ClientModInitializer {
+
+   @Override
+   public void onInitializeClient() {
+
+   }
+}
